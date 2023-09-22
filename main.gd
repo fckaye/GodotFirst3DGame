bd811303faa1c0732daa49b4b3567871638c071b
Fernando Caye <fdcaye@gmail.com>
2023-09-22 12:43:23 +0900

    Basic UI and background music.

--- main.gd
+++ main.gd
@@ -2,6 +2,9 @@ extends Node
 
 @export var mob_scene: PackedScene
 
+func _ready():
+	$UserInterface/Retry.hide()
+	
 func _on_mob_timer_timeout():
 	# Create a new instance of Mob
 	var mob = mob_scene.instantiate()
@@ -17,7 +20,16 @@ func _on_mob_timer_timeout():
 	
 	# Spawn mob by adding to Main scene
 	add_child(mob)
+	
+	# Connect the mob to the score label to update score upon squashing
+	mob.squashed.connect($UserInterface/ScoreLabel._on_mob_squashed.bind())
 
 
 func _on_player_hit():
 	$MobTimer.stop()
+	$UserInterface/Retry.show()
+	
+func _unhandled_input(event):
+	if event.is_action_pressed("ui_accept") and $UserInterface/Retry.visible:
+		# Restart the current scene
+		get_tree().reload_current_scene()
